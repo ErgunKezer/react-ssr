9672d4450636b2b4eaeed9d206ce887c04a2a7d5
Ergun Kezer <ekzr34@gmail.com>
2020-05-20 03:46:57 +0300

    packages updated

--- webpack.server.js
+++ webpack.server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 module.exports = {
+  mode: 'development',
   // inform webpack that we are building a bundle for nodeJS, rather than for the browser
   target: 'node',
   // tell webpack the root file of our server application
